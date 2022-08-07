7a80c92527060b3ec733d5db2109fa24846b00c0
vickysingh1998 <vickysingh8697@gmail.com>
2022-08-07 23:39:27 +0530

    added: a simple LinkedList program

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList <Integer> number = new LinkedList<>();
+        number.add(56);
+        number.add(30);
+        number.add(70);
+
+        System.out.println("Linked List Is"+number);
+    }
+}
